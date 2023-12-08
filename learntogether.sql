5a1cd965faccf570d619f6348826ce9a30f5674b
SamirKhan21860 <msamirkhan561@gmail.com>
2023-12-08 21:30:23 +0500

    adding assignments table into database

--- learntogether.sql
+++ learntogether.sql
@@ -10,6 +10,37 @@ CREATE TABLE students (
     profile_image_url TEXT
 );
 
+-- Add this to learntogether.sql
+CREATE TABLE assignments (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    class_name TEXT,
+    assignment_name TEXT,
+    deadline DATETIME,
+    reminder_time DATETIME,
+    FOREIGN KEY (user_id) REFERENCES students(id)
+);
+
+
+-- -- Table for Classes
+-- CREATE TABLE classes (
+--     id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     user_id INTEGER,
+--     class_name TEXT,
+--     schedule TEXT,
+--     UNIQUE(user_id, class_name)
+-- );
+
+-- -- Table for Assignments
+-- CREATE TABLE assignments (
+--     id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     user_id INTEGER,
+--     class_id INTEGER,
+--     assignment_name TEXT,
+--     deadline DATETIME,
+--     UNIQUE(user_id, assignment_name)
+-- );
+
 -- .save and .read: Save and load SQL commands from a file.
 
 -- .save filename: Save the current SQLite3 session to a file.
