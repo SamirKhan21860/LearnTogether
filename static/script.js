bbdcee07ca0c87c85a551d0cf55b34a86acc8636
SamirKhan21860 <msamirkhan561@gmail.com>
2023-12-14 01:01:26 +0500

    all conflicts are resolved

--- static/script.js
+++ static/script.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-// Define a custom function to assign category-specific styles
-function get_message_style_class(category) {
-    // Implement your logic to determine the category and return the corresponding class
-    console.log("Loaded successfully!")
-    switch (category) {
-        case 'error':
-            return 'bg-red-500 text-white';
-        case 'info':
-            return 'bg-blue-500 text-white';
-        case 'success':
-            return 'bg-green-500 text-white';
-        // Add more cases as needed
-        default:
-            return 'bg-gray-500 text-white'; // Default style
-    }
-}
-
-// Even listener for menu button
-document.getElementById('burger-menu').addEventListener('click', function () {
-    document.getElementById('burger-menu-content').classList.toggle('hidden');
-});
-=======
 // // Define a custom function to assign category-specific styles
 // function message_style(category) {
 //     // Implement your logic to determine the category and return the corresponding class
@@ -71,4 +48,3 @@ document.addEventListener('DOMContentLoaded', function () {
 
     }
 });
->>>>>>> flash_messages
