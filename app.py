075e70d1d150275b36d8c934ebd61550f4471383
SamirKhan21860 <msamirkhan561@gmail.com>
2023-12-31 13:33:24 +0500

    database updated from flash_messages branch

--- app.py
+++ app.py
@@ -25,19 +25,16 @@ db = SQL("sqlite:///learntogether.db")
 @login_required
 def index():
     # Retrieve assignments for the logged-in student
-    if session["user_id"]:
-        assignment = db.execute("SELECT * FROM assignment WHERE student_id = ?", session["user_id"])
-        # Render the homepage template with assignment data
-        return render_template("index.html", assignment=assignment)
-
-    return render_template("index.html")
+    assignment = db.execute("SELECT * FROM assignment WHERE student_id = ?", session["user_id"])
+    # Render the homepage template with assignment data
+    return render_template("index.html", assignment=assignment)
 
 # Define route for user signup
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
     # Clear any existing session data
     session.clear()
-
+    
     # Handle POST request for user signup
     if request.method == "POST":
         # Extract user input from the form
@@ -45,17 +42,17 @@ def signup():
         email = request.form.get("email")
         password = request.form.get("password")
         class_name = request.form.get("class")
-
+        
         # Check if all required fields are provided
         result = check_required_fields({"Full Name": full_name, "Email": email, "Password": password, "Class Name": class_name}, current_page="signup")
         if result:
             return result
-
+        
         # Validate the format of the class name
         if not validate_class_name(class_name):
             flash_message("Class Name must adhere to the specified convention. i.e(CS50, CS or any acronym)", category='info')
             return render_template("signup.html")
-
+        
         # Check if the provided email already exists in the database
         email_exists = db.execute("SELECT * FROM students WHERE email = ?", email)
         if email_exists:
@@ -64,14 +61,14 @@ def signup():
 
         # Hash the password before storing it in the database
         hash_password = generate_password_hash(password)
-
+        
         # Insert user data into the database
         db.execute("INSERT INTO students (full_name, email, password, class_name, role) VALUES (?, ?, ?, ?, ?)", full_name, email, hash_password, class_name, 'student')
-
+        
         # Display a success message and redirect to the login page
         flash_message("Signup successful!", category='success')
         return render_template("login.html")
-
+    
     # Render the signup page for GET requests
     return render_template("signup.html")
 
@@ -80,36 +77,36 @@ def signup():
 def login():
     # Clear any existing session data
     session.clear()
-
+    
     # Display a logout message if the request includes a logout parameter
     if request.args.get('logout'):
         flash_message("You're Logout!", category='success')
-
+    
     # Handle POST request for user login
     if request.method == "POST":
         # Extract user input from the form
         email = request.form.get("email")
         password = request.form.get("password")
-
+        
         # Check if email and password are provided
         if not email or not password:
             flash_message("Provide required Fields to Log In!", category='error')
             return render_template("login.html")
-
+        
         # Retrieve user data from the database based on the provided email
         student = db.execute("SELECT * FROM students WHERE email = ?", email)
-
+        
         # Check if the user exists and the password is correct
         if not student or not check_password_hash(student[0]["password"], password):
             flash_message("Invalid email or password!", category='error')
             return render_template("login.html")
-
+        
         # Store the user ID in the session for authentication
         session["user_id"] = student[0]["id"]
         # Display a success message and redirect to the homepage
         flash_message("Log In Successful!", category='success')
         return redirect(url_for("index"))
-
+    
     # Render the login page for GET requests
     return render_template("login.html")
 
@@ -138,12 +135,12 @@ def assign():
         name = request.form.get('assignment_name')
         subject = request.form.get('subject')
         description = request.form.get('assignment_description')
-
+        
         # Check if assignment details are provided
         if name and subject and description:
             # Insert assignment data into the database
             db.execute("INSERT INTO assignment (student_id, name, subject, description) VALUES (?, ?, ?, ?)", session["user_id"], name, subject, description)
-
+    
     # Redirect to the homepage
     return redirect("/")
 
@@ -171,19 +168,19 @@ def complaint():
         category = request.form.get("category")
         subject = request.form.get("subject")
         description = request.form.get("description")
-
+    
         # Check if all required fields are provided
         result = check_required_fields({"Submission Type": submission_type, "Category": category, "Subject": subject, "Description": description}, current_page="complaint_or_request")
         if result:
             return result
-
+        
         # Insert complaint or request data into the database
         db.execute("INSERT INTO complaints_requests (student_id, submission_type, category, subject, description) VALUES (?, ?, ?, ?, ?)", session["user_id"], submission_type, category, subject, description)
-
+        
         # Display a success message and redirect to the homepage
         flash_message("Your Complaint or request submitted successfully!", category="success")
         return redirect(url_for("index"))
-
+    
     # Render the complaints or requests form for GET requests
     return render_template("complaint_or_request.html")
 
